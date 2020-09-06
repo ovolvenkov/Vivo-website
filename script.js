27a56d24711c19b18c74fc781564c3635b0eabfd
ovolvenkov <ovolvenkov@gmail.com>
2020-09-07 00:10:01 +0300

    smoth scrolling anchor

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
 'use strict'
 
-function playVideo() {
-	const videoButton = document.querySelector('.video_btn');
-	const videoFrame = document.querySelector('.video_frame');
-
-
-
-	videoButton.addEventListener('click', (e)=>{
-
-		videoFrame.style.display = 'block';
-
-	})
-}
-
-
-
-
-
 
 function reduceMenuSize() {
 	const headerTop = document.querySelector('.header_top');
-	document.addEventListener('scroll',(e)=>{
-		if(window.pageYOffset > 0){
+	document.addEventListener('scroll', (e) => {
+		if (window.pageYOffset > 0) {
 			headerTop.style.paddingTop = '10px';
 			headerTop.style.transition = 'padding-top 0.3s ease-in-out';
 		} else if (window.pageYOffset == 0) {
@@ -41,19 +24,18 @@ function animateAboutSection() {
 	const aboutUs = document.querySelector('.about_us');
 
 	document.addEventListener('scroll', (e)=>{
-		if(window.pageYOffset > 300) {
+		if (window.pageYOffset > 300) {
 			aboutPhoto.classList.add('active_about_photo');
 			about_info.style.transform = 'scale(1)'
-			aboutUs.style.transform = 'translateY(0%)'
+			//aboutUs.style.transform = 'translateY(0%)'
 
 		}
-		else if(window.pageYOffset < 300) {
+		else if (window.pageYOffset < 300) {
 			aboutPhoto.classList.remove('active_about_photo')
 			about_info.style.transform = 'scale(0)'
-						aboutUs.style.transform = 'translateY(100%)'
+						//aboutUs.style.transform = 'translateY(100%)'
 		}
 	})
-
 }
 
 animateAboutSection()
@@ -61,44 +43,93 @@ animateAboutSection()
 
 
 
-function modal() {
+function modalVideo() {
 	const videoButton = document.querySelector('.video_btn');
 	const body = document.querySelector('body');
 	const modalContainer = document.querySelector('#modal-container');
 	const modal = document.querySelector('.modal');
-
-
-
 
 	videoButton.addEventListener('click',( e => {
 		modalContainer.style.display = 'table'
 		modalContainer.removeAttribute('class');
 		modalContainer.classList.add('one');
 		body.classList.add('modal-active');
-		modal.innerHTML = '<iframe width="1015" height="523" src="https://www.youtube.com/embed/-yVqM6WnPs0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-
-
+		modal.innerHTML = `<iframe width="1015" height="523" 
+							src="https://www.youtube.com/embed/-yVqM6WnPs0" 
+							frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; 
+							picture-in-picture" allowfullscreen></iframe>`;
 	}))
 
 	modalContainer.addEventListener('click',( e => {
 		modalContainer.classList.add('out');
 		body.classList.remove('modal-active');
 		modalContainer.style.transform = 'scale(0)';
-
-
 		modal.innerHTML = '';
-
-		
-
 	}))
 }
 
-modal()
+modalVideo()
 
 
-function animateSlideLine() {
 
+
+
+function smoothScrolling() {
+	document.addEventListener('click', function(e) {
+		let anchors = document.querySelectorAll('a[href^="#"]');
+			for (let i = 0; i < anchors.length; i++) {
+				if(anchors[i] === e.target) {
+					e.preventDefault();
+					let selector = e.target.getAttribute('href');
+					let elem = document.querySelector(`${selector}`);
+					console.log(elem.offsetTop)
+					if (selector === '#about') {
+						window.scrollTo(0,elem.offsetTop - 180)
+					} else if(selector === '#our_services') {
+						window.scrollTo(0,elem.offsetTop - 70)
+					} else if(selector === '#contacts'){
+						window.scrollTo(0,elem.offsetTop - 240)
+					} else {
+						window.scrollTo(0,elem.offsetTop - 80)
+					}
+				}
+			}
+	})
 }
+
+//smoothScrolling()
+
+// smooth anchor scrolling
+
+document.addEventListener('click', function(event){
+	if(event.target.classList.contains('smooth')){
+		if(window.innerWidth > 1024 || screen.width > 1024 ){
+			event.preventDefault();
+		}
+		let targetAnchor = event.target.getAttribute('href');
+		let targetElem = document.querySelector(targetAnchor);
+		let targetTop = targetElem.offsetTop;
+		window.scrollTo({
+			left: 0,
+			top: targetTop,
+			behavior: 'smooth'
+		});
+	}
+// scroll totop
+	if(event.target.classList.contains('top_link') ||
+	 event.target.parentNode.classList.contains('top_link')){
+		window.scrollTo({
+			left: 0,
+			top: 0,
+			behavior: 'smooth'
+		});
+		// tip for IOS Safari
+		if(window.innerWidth <= 1024 || screen.width <= 1024 ){
+			document.body.scrollTop = 0; // For Safari
+			window.location.hash = 'htop';
+		}
+	}
+});
 
 
 
