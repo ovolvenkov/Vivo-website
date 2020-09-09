a644118300a30c5f98a039ffd82b5f4f780c6e20
ovolvenkov <ovolvenkov@gmail.com>
2020-09-09 20:29:56 +0300

    make adaptive

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function reduceMenuSize() {
 	})
 }
 
-reduceMenuSize()
+if(document.documentElement.clientWidth > 375) reduceMenuSize()
 
 
 
@@ -27,7 +27,7 @@ function animate() {
 
 
 	document.addEventListener('scroll', (e)=>{
-		console.log(window.pageYOffset)
+		//console.log(window.pageYOffset)
 		if (window.pageYOffset > 300) {
 			aboutPhoto.classList.add('active_about_photo');
 			about_info.style.transform = 'scale(1)'
@@ -84,7 +84,7 @@ function animate() {
  	 });
 }
 
-animate()
+if(document.documentElement.clientWidth > 375) animate();
 
 
 
@@ -117,80 +117,88 @@ function modalVideo() {
 modalVideo()
 
 
-//smoothScrolling()
-
 // smooth anchor scrolling
 
-document.addEventListener('click', function(event){
-	if(event.target.classList.contains('smooth')){
-		if(window.innerWidth > 1024 || screen.width > 1024 ){
+function smoothScrolling() {
+
+	document.addEventListener('click', function(event){
+		if(event.target.classList.contains('smooth')){
 			event.preventDefault();
+			let targetAnchor = event.target.getAttribute('href');
+			let targetElem = document.querySelector(targetAnchor);
+			let targetTop = targetElem.offsetTop;
+
+			if (targetAnchor === '#about') {
+				window.scrollTo({
+					left: 0,
+					top: targetTop - 120,
+					behavior: 'smooth'
+				})
+			} else if(targetAnchor === '#our_services') {
+				window.scrollTo({
+					left: 0,
+					top: targetTop - 70,
+					behavior: 'smooth'
+				})
+			} else if(targetAnchor === '#contacts'){
+				window.scrollTo({
+					left: 0,
+					top: targetTop - 240,
+					behavior: 'smooth'
+				})
+			} else {
+				window.scrollTo({
+					left: 0,
+					top: targetTop - 80,
+					behavior: 'smooth'
+				})
+			}
 		}
-		let targetAnchor = event.target.getAttribute('href');
-		let targetElem = document.querySelector(targetAnchor);
-		let targetTop = targetElem.offsetTop;
-
-		if (targetAnchor === '#about') {
+	// scroll totop
+		if(event.target.classList.contains('logo') || event.target.parentNode.classList.contains('logo')){
 			window.scrollTo({
 				left: 0,
-				top: targetTop - 180,
+				top: 0,
 				behavior: 'smooth'
-			})
-		} else if(targetAnchor === '#our_services') {
-			window.scrollTo({
-				left: 0,
-				top: targetTop - 70,
-				behavior: 'smooth'
-			})
-		} else if(targetAnchor === '#contacts'){
-			window.scrollTo({
-				left: 0,
-				top: targetTop - 240,
-				behavior: 'smooth'
-			})
-		} else {
-			window.scrollTo({
-				left: 0,
-				top: targetTop - 80,
-				behavior: 'smooth'
-			})
+			});
+			// tip for IOS Safari
+			if(window.innerWidth <= 1024 || screen.width <= 1024 ){
+				document.body.scrollTop = 0; // For Safari
+				window.location.hash = 'htop';
+			}
 		}
-	}
-// scroll totop
-	if(event.target.classList.contains('logo') || event.target.parentNode.classList.contains('logo')){
-		window.scrollTo({
-			left: 0,
-			top: 0,
-			behavior: 'smooth'
-		});
-		// tip for IOS Safari
-		if(window.innerWidth <= 1024 || screen.width <= 1024 ){
-			document.body.scrollTop = 0; // For Safari
-			window.location.hash = 'htop';
+
+	//btn scroll to bottom
+		if (event.target.classList.contains('top_link') || event.target.parentNode.classList.contains('top_link')) {
+			let targetElem = document.querySelector('#about');
+			window.scrollTo({
+				left: 0,
+				top: targetElem.offsetTop - 180,
+				behavior: 'smooth'
+			})
+
+
 		}
-	}
-
-//btn scroll to bottom
-	if (event.target.classList.contains('top_link') || event.target.parentNode.classList.contains('top_link')) {
-		let targetElem = document.querySelector('#about');
-		window.scrollTo({
-			left: 0,
-			top: targetElem.offsetTop - 180,
-			behavior: 'smooth'
-		})
 
 
-	}
+	});
+}
 
-
-});
-
+smoothScrolling()
 
 function openCloseProduct() {
 	const models = document.querySelectorAll('.models');
 
 	document.addEventListener('click', function(e) {
+		models.forEach(item => item.classList.remove('models_active'))
+		if (e.target.classList.contains('icon_text')) {
+			e.target.nextElementSibling.classList.toggle('models_active');
+		} 
 
+	})
+
+	//for mobile browsers event
+	document.addEventListener('touchstart', function(e) {
 		models.forEach(item => item.classList.remove('models_active'))
 		if (e.target.classList.contains('icon_text')) {
 			e.target.nextElementSibling.classList.toggle('models_active');
@@ -202,6 +210,29 @@ function openCloseProduct() {
 openCloseProduct()
 
 
+
+function changeHeaderBackgroung() {
+		let backgroundImg = document.querySelector('.background_img');
+		if (document.documentElement.clientWidth <= 768 && document.documentElement.clientWidth >= 376) {
+			backgroundImg.innerHTML = "<img src='img/header_bg_768.png' alt='background image'>";
+		} else if (document.documentElement.clientWidth < 376) {
+			backgroundImg.innerHTML = "<img src='img/header_bg_768.png' alt='background image'>";
+		} else {
+			backgroundImg.innerHTML = "<img src='img/header_bg_img.png' alt='background image'>";
+		}
+
+		window.addEventListener('resize', (e)=>{
+			if (document.documentElement.clientWidth <= 768 && document.documentElement.clientWidth >= 376) {
+				backgroundImg.innerHTML = "<img src='img/header_bg_768.png' alt='background image'>";
+			} else if (document.documentElement.clientWidth < 376) {
+				backgroundImg.innerHTML = "<img src='img/header_bg_768.png' alt='background image'>";
+			} else {
+				backgroundImg.innerHTML = "<img src='img/header_bg_img.png' alt='background image'>";
+			}
+		})
+}
+
+changeHeaderBackgroung()
 
 /*function menu() {
 	let menuContainer = document.querySelector('.menu .container');
